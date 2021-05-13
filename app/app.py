a732be11accbe49e8b4a07f62132f92d3166772d
Alice Allafort <allafort.alice@gmail.com>
2021-05-13 11:55:29 -0700

    full website; add data

--- app/app.py
+++ app/app.py
@@ -13,23 +13,29 @@ app.config['img_dir'] = img_dir
 
 app.vars = {'ingredients_kw': ''}
 
+cuisine_list_reduced = ['italian', 'moroccan', 'thai', 'french', 'southern_us', 'indian', 'greek', 'mexican',
+                        'japanese']
+cuisine_list_reduced_names = ['Italian', 'Moroccan', 'Thai', 'French', 'Southern', 'Indian', 'Greek', 'Mexican',
+                              'Japanese']
+
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         form = {}
-        form['cuisine_list'] = cuisine_list
+        form['cuisine_list'] = cuisine_list_reduced
+        form['cuisine_list_names'] = cuisine_list_reduced_names
         return render_template('index.html', **form)
     else:
         app.vars['ingredients_kw'] = request.form['ingredients_kw']
         app.vars['cuisines_selected'] = []
-        for cui in cuisine_list:
+        for cui in cuisine_list_reduced:
             print(cui)
             if request.form.get(cui) != None:
                 print(cui, 'selected')
                 app.vars['cuisines_selected'].append(cui)
 
-        recipe_list = get_recipes(df, kw=app.vars['ingredients_kw'], cuis=app.vars['cuisines_selected'] )
+        recipe_list = get_recipes(df, kw=app.vars['ingredients_kw'], cuis=app.vars['cuisines_selected'])
 
         results = {}
         results['ingredients_kw'] = app.vars['ingredients_kw']
@@ -42,14 +48,17 @@ def index():
 @app.route('/recipe_<recipe_id>')
 def recipe(recipe_id):
     results = {}
+    results['recipe_img']=None
     results['recipe_data'] = get_recipes(df, rec_id=recipe_id)[0]
-
+    results['recipe_id'] = recipe_id
+    if recipe_id == '19016':
+        results['recipe_img'] = 'https://assets.epicurious.com/photos/55d74922edfa3b005396a03a/6:4/w_620%2Ch_413/238699_salmon-chowder_6x4.jpg'
     return render_template('recipe.html', **results)
 
 
-@app.route('/home')
+@app.route('/welcome')
 def home():
-    full_filename = os.path.join(app.config['img_dir'], 'pexels-lukas-349609.jpg')
+    full_filename = os.path.join(app.config['img_dir'], 'welcome.jpg')
     template_context = dict(background=full_filename)
     return render_template('home.html', **template_context)
 
@@ -100,7 +109,9 @@ def get_recipes(df, rec_id=None, kw=None, cuis=[]):
         mask = df.recipe_id == int(rec_id)
     df_sel = df[mask]
     if cuis:
+        print('CUISINES',cuis,'KW',kw)
         df_sel = sort_cuis(df_sel, cuis)
+    df_sel = df_sel.drop_duplicates(subset="title")
     return df_sel[['recipe_id', 'title', 'ing_cleaned', 'ingredients', 'directions', 'cuisine']].to_dict('records')
 
 
